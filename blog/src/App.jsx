443547d207a75ceacd96538c0ff82ecba6ed218c
Subham15112001 <sinchan889377888@gmail.com>
2024-03-17 21:21:05 +0530

    completed

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,13 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
 import './App.css'
+import {login,logout} from "./store/authSlice.js"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-     <div>blog app</div>
-    </>
-  )
+  
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  },[])
+
+  return (!loading)? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* react router dom needed to be handle */}
+          <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ): null;
 }
 
 export default App
